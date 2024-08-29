7cc91bd79ccd81c23b0968f883f1dc07171e280b
silvester <silvester>
2024-08-29 14:15:14 +0200

    Improve pawn promotion

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import { Game } from './classes/Game.js';
-import { Move } from './classes/Move.js';
 import { Player, Strategy } from './classes/Player.js';
 import { Color, Piece, King, Queen, Rook, Bishop, Knight, Pawn } from './classes/Pieces.js';
 
@@ -8,9 +7,8 @@ const canvasContext = canvas.getContext('2d');
 
 const chessGame = new Game();
 let allPieces: Piece[];
-let whitePlayer = new Player(Color.WHITE, Strategy.MATERIAL_FIRST);
-let blackPlayer = new Player(Color.BLACK, Strategy.MATERIAL_FIRST);
-let weHaveAWinner = false;
+let whitePlayer = new Player(Color.WHITE, Strategy.RANDOM);
+let blackPlayer = new Player(Color.BLACK, Strategy.RANDOM);
 
 const returnPieceFromStartingPosition = (x: number, y: number) => {
   if (y === 1) {
@@ -32,52 +30,17 @@ const returnPieceFromStartingPosition = (x: number, y: number) => {
 
 allPieces = Array.from(Array(32), (_, number) => returnPieceFromStartingPosition(number % 8, number < 16 ? Math.floor(number / 8) : 4 + Math.floor(number / 8)));
 
-const movePiece = (move: Move): void => {
-  let allyPieceIndex = allPieces.findIndex(p => p.positionX === move.startX && p.positionY === move.startY);
-  let enemyPieceIndex = allPieces.findIndex(p => p.positionX === move.endX && p.positionY === move.endY);
-
-  if (allPieces[allyPieceIndex]) {
-    allPieces[allyPieceIndex].positionX = move.endX;
-    allPieces[allyPieceIndex].positionY = move.endY;
-    if (allPieces[allyPieceIndex] instanceof Pawn) {
-      allPieces[allyPieceIndex].firstMove = false;
-      if ((allPieces[allyPieceIndex].color === Color.WHITE && allPieces[allyPieceIndex].positionY === 0) || (allPieces[allyPieceIndex].color === Color.BLACK && allPieces[allyPieceIndex].positionY === 7)) {
-        allPieces[allyPieceIndex] = new Queen(allPieces[allyPieceIndex].positionX, allPieces[allyPieceIndex].positionY, allPieces[allyPieceIndex].color);
-      }
-    }
-    if (enemyPieceIndex > -1) {
-      if (allPieces[enemyPieceIndex] instanceof King) {
-        console.log(allPieces[allyPieceIndex].color === Color.WHITE ? "White" : "Black", " WON !!!");
-        weHaveAWinner = true;
-      }
-      allPieces.splice(enemyPieceIndex, 1);
-    }
-  }
-};
-
 async function playerDelay(p: Player, delay: number): Promise<boolean> {
   return new Promise(resolve => {
-    setTimeout(() => resolve(p.play(allPieces, movePiece)), delay);
+    setTimeout(() => resolve(p.play(allPieces)), delay);
   });
 }
 
 let button = document.querySelector('#start');
 
 async function startGame() {
-  let whiteCanPlay = true;
-  let blackCanPlay = true;
-  
-  while (whiteCanPlay && blackCanPlay && !weHaveAWinner) {
-    whiteCanPlay = await playerDelay(whitePlayer, 500);
-    if (!weHaveAWinner) {
-      blackCanPlay = await playerDelay(blackPlayer, 500);
-    }
-  }
-  if (!whiteCanPlay) {
-    console.log("White can't play");
-  }
-  if (!blackCanPlay) {
-    console.log("Black can't play");
+  while (await playerDelay(whitePlayer, 500) && await playerDelay(blackPlayer, 500)) {
+    
   }
 }
 
--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,4 +1,4 @@
-import { Color, Piece } from './Pieces.js';
+import { Color, Piece, King, Queen, Rook, Bishop, Knight, Pawn } from './Pieces.js';
 import { Move } from './Move.js';
 
 export enum Strategy { RANDOM, MATERIAL_FIRST }
@@ -16,19 +16,56 @@ export class Player {
     this.strategy = s;
   }
 
-  play(allPieces: Piece[], moveCallback: Function): boolean {
+  play(allPieces: Piece[]): boolean {
     let moves: Move[] = [];
     moves = allPieces.filter(p => p.color === this.color).reduce((acc, cur) => acc.concat(cur.possibleMoves(allPieces)), moves);
 
     if (moves.length > 0) {
       if (this.strategy === Strategy.RANDOM) {
-        moveCallback(moves[Math.floor(Math.random() * moves.length)]);
+        return this.movePiece(allPieces, moves[Math.floor(Math.random() * moves.length)]);
       } else if (this.strategy === Strategy.MATERIAL_FIRST) {
         moves.sort((a, b) => getMoveValue(allPieces, b) - getMoveValue(allPieces, a));
-        moveCallback(moves[0]);
+        return this.movePiece(allPieces, moves[0]);
       }
-      return true;
     }
+    console.log(this.color === Color.WHITE ? "White" : "Black", " can't play");
     return false;
+  }
+
+  movePiece(allPieces: Piece[], move: Move): boolean {
+    let allyPieceIndex = allPieces.findIndex(p => p.positionX === move.startX && p.positionY === move.startY);
+    let enemyPieceIndex = allPieces.findIndex(p => p.positionX === move.endX && p.positionY === move.endY);
+    let keepOnPlaying = false;
+  
+    if (allPieces[allyPieceIndex]) {
+      allPieces[allyPieceIndex].positionX = move.endX;
+      allPieces[allyPieceIndex].positionY = move.endY;
+      if (allPieces[allyPieceIndex] instanceof Pawn) {
+        allPieces[allyPieceIndex].firstMove = false;
+        if ((allPieces[allyPieceIndex].color === Color.WHITE && allPieces[allyPieceIndex].positionY === 0) || (allPieces[allyPieceIndex].color === Color.BLACK && allPieces[allyPieceIndex].positionY === 7)) {
+          allPieces[allyPieceIndex] = this.promote(allPieces[allyPieceIndex].positionX, allPieces[allyPieceIndex].positionY);
+        }
+      }
+      keepOnPlaying =  true;
+      if (enemyPieceIndex > -1) {
+        if (allPieces[enemyPieceIndex] instanceof King) {
+          console.log(this.color === Color.WHITE ? "White" : "Black", " WON !!!");
+          keepOnPlaying =  false;
+        }
+        allPieces.splice(enemyPieceIndex, 1);
+      }
+    } else {
+      console.log(this.color === Color.WHITE ? "White" : "Black", " doesn't have a piece at that position");
+    }
+    return keepOnPlaying;
+  }
+
+  promote(x: number, y: number): Piece {
+    let possiblePieces = [new Queen(x, y, this.color), new Rook(x, y, this.color), new Bishop(x, y, this.color), new Knight(x, y, this.color)];
+
+    if (this.strategy === Strategy.RANDOM) {
+      return possiblePieces[Math.floor(Math.random() * possiblePieces.length)];
+    }
+    return possiblePieces[0];
   }
 }
